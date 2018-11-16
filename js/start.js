e84335efcf1bbc9e0e93bd63b4b8693a228b545e
Dmitriy <ivldv@mail.ru>
2018-11-16 12:19:08 +0300

    5

--- js/start.js
+++ js/start.js
@@ -1,6 +1,6 @@
+"use strict";
 let num = 33721;
-let digits = num.toString().split('');
-let mul = 1;
-digits.forEach((item) => mul *= item);
-console.log(mul);
-console.log((mul ** 3).toString().slice(0, 2));
+let multiplication = 1;
+num.toString().split('').forEach((item) => multiplication *= item);
+console.log(multiplication);
+document.write((multiplication ** 3).toString().slice(0, 2));
